e5fb2b40f566c8d00467a5103efdec637d456643
Guilherme Heimbecker Eisfeld <guilherme.eisfeld@compasso.com.br>
2021-05-26 21:14:59 -0300

    Fixing Pages

---
+++ pages/countries/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Head from "next/head";
+import Image from "next/image";
+
+
+export default function Countries() {
+  return (
+    <>
+      <Maps />
+    </>
+  );
+}
